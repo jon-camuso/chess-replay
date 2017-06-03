9b1df953c10288d7cc8a7b13634eabdaaad59f46
Denis Michael <geek.denis17@gmail.com>
2017-06-03 13:31:59 -0400

    Set Cell Ids

--- chessReplay.js
+++ chessReplay.js
@@ -2,6 +2,7 @@ var container = document.querySelector('#chessReplayApp');
 var chessBoard = document.createElement('table');
 
 const chessBoardTileLength = 8;
+const columnLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
 var rowIndex = 0;
 var columnIndex  = 0;
@@ -10,8 +11,10 @@ for(rowIndex; rowIndex < chessBoardTileLength; ++rowIndex){
     var chessBoardRow = chessBoard.insertRow(rowIndex);
     chessBoardRow.id = chessBoardTileLength - rowIndex;
     for(columnIndex = 0; columnIndex < chessBoardTileLength; ++columnIndex){
-         chessBoardRow.insertCell(columnIndex);
+        var chessBoardCell = chessBoardRow.insertCell(columnIndex);
+        chessBoardCell.id = columnLetters [columnIndex] + chessBoardRow.id;
     }
 }
+
 
 container.appendChild(chessBoard);
