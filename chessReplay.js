3369006403d8f71f0716ce6fe7d935a43e37d8ac
Jon Camuso <joncamuso@Jons-iMac.local>
2017-06-03 16:44:42 -0400

    Added starting pieces object

--- chessReplay.js
+++ chessReplay.js
@@ -6,7 +6,7 @@ var pieceMap = {};
 const chessBoardTileLength = 8;
 const columnLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 const colors = ['white', 'black'];
-const pieces = ['King', 'Queen', 'Bishop', 'Knight', 'Rook', 'Pawn']
+const pieceTypes = ['King', 'Queen', 'Bishop', 'Knight', 'Rook', 'Pawn']
 
 var rowIndex = 0;
 var columnIndex  = 0;
@@ -23,8 +23,8 @@ for(rowIndex; rowIndex < chessBoardTileLength; ++rowIndex){
 var colorIndex = 0;
 var pieceIndex = 0;
 for(colorIndex; colorIndex < colors.length; ++colorIndex){
-    for(pieceIndex = 0; pieceIndex < pieces.length; ++pieceIndex){
-        pieceMap[colors[colorIndex] + pieces[pieceIndex]] = {};
+    for(pieceIndex = 0; pieceIndex < pieceTypes.length; ++pieceIndex){
+        pieceMap[colors[colorIndex] + pieceTypes[pieceIndex]] = {};
     }
 }
 console.log(JSON.stringify(pieceMap));
@@ -38,3 +38,15 @@ for(var name in pieceMap){
     container.appendChild(buildPiece(name));
 }
 container.appendChild(chessBoard);
+
+
+var startingPeices = {
+    blackKing:{
+        color: 'black',
+        type: 'King',
+        postion: {
+            column: 'e',
+            row: 8
+        }
+    }
+}
