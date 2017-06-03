a753836cfcbad1e82b32ad61abfa023d75ad1cc4
Joseph Camuso <joseph.camuso@exechos.com>
2017-06-02 20:39:45 -0400

    Changed let to var for older versions of Safari

--- chessReplay.js
+++ chessReplay.js
@@ -1,13 +1,13 @@
-let container = document.querySelector('#chessReplayApp');
-let chessBoard = document.createElement('table');
+var container = document.querySelector('#chessReplayApp');
+var chessBoard = document.createElement('table');
 
 const chessBoardTileLength = 8;
 
-let rowIndex = 0;
-let columnIndex  = 0;
+var rowIndex = 0;
+var columnIndex  = 0;
 
 for(rowIndex; rowIndex < chessBoardTileLength; ++rowIndex){
-    let chessBoardRow = chessBoard.insertRow(rowIndex);
+    var chessBoardRow = chessBoard.insertRow(rowIndex);
     chessBoardRow.id = chessBoardTileLength - rowIndex;
     for(columnIndex = 0; columnIndex < chessBoardTileLength; ++columnIndex){
          chessBoardRow.insertCell(columnIndex);
