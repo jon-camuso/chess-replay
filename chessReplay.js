af78d01bf1d605e3d93a2e39091ea20dfe646732
Denis Michael <geek.denis17@gmail.com>
2017-06-02 20:05:58 -0400

    set row ids

--- chessReplay.js
+++ chessReplay.js
@@ -1,1 +1,17 @@
-console.log('hi');
+let container = document.querySelector('#chessReplayApp');
+let chessBoard = document.createElement('table');
+
+const chessBoardTileLength = 8;
+
+let rowIndex = 0;
+let columnIndex  = 0;
+
+for(rowIndex; rowIndex < chessBoardTileLength; ++rowIndex){
+    let chessBoardRow = chessBoard.insertRow(rowIndex);
+    chessBoardRow.id = chessBoardTileLength - rowIndex;
+    for(columnIndex = 0; columnIndex < chessBoardTileLength; ++columnIndex){
+         chessBoardRow.insertCell(columnIndex);
+    }
+}
+
+container.appendChild(chessBoard);
