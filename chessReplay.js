bc49e27762bc7f41398eaa8db9b8c14c5fa66633
Denis Michael <geek.denis17@gmail.com>
2017-06-03 16:26:55 -0400

    added function to build pieces

--- chessReplay.js
+++ chessReplay.js
@@ -28,32 +28,13 @@ for(colorIndex; colorIndex < colors.length; ++colorIndex){
     }
 }
 console.log(JSON.stringify(pieceMap));
+function buildPiece(name){
+    var piece = document.createElement('div');
+    piece.classList.add('piece', name);
+    return piece;
+}
 
-var whiteKing = document.createElement('div');
-whiteKing.classList.add('piece', 'whiteKing');
-var whiteQueen = document.createElement('div');
-whiteQueen.classList.add('piece', 'whiteQueen');
-var whiteBishop = document.createElement('div');
-whiteBishop.classList.add('piece', 'whiteBishop');
-var whiteKnight = document.createElement('div');
-whiteKnight.classList.add('piece', 'whiteKnight');
-var whiteRook = document.createElement('div');
-whiteRook.classList.add('piece', 'whiteRook');
-var whitePawn = document.createElement('div');
-whitePawn.classList.add('piece', 'whitePawn');
-
-var blackKing = document.createElement('div');
-blackKing.classList.add('piece', 'blackKing');
-var blackQueen = document.createElement('div');
-blackQueen.classList.add('piece', 'blackQueen');
-var blackBishop = document.createElement('div');
-blackBishop.classList.add('piece', 'blackBishop');
-var blackKnight = document.createElement('div');
-blackKnight.classList.add('piece', 'blackKnight');
-var blackRook = document.createElement('div');
-blackRook.classList.add('piece', 'blackRook');
-var blackPawn = document.createElement('div');
-blackPawn.classList.add('piece', 'blackPawn');
- 
+for(var name in pieceMap){
+    container.appendChild(buildPiece(name));
+}
 container.appendChild(chessBoard);
-container.appendChild(whiteKing);
